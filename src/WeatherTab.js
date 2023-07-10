2bf6d1bb13a551fa96e0926ca0b970026550af5c
dagifer <125514178+dagifer@users.noreply.github.com>
2023-07-10 15:11:04 +0100

    delete logo192/512

--- src/WeatherTab.js
+++ src/WeatherTab.js
@@ -10,7 +10,7 @@ export default function WeatherTab(props) {
         <li>
           <FormattedDate date={props.data.date} />
         </li>
-        <li>{props.data.description}</li>
+        <li className="text-capitalize">{props.data.description}</li>
       </ul>
       <div className="row">
         <div className="col-6">
