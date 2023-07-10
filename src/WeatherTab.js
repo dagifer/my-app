4d9006d82f3223bd03d7e88e3fbfeaa4f090d6ea
dagifer <125514178+dagifer@users.noreply.github.com>
2023-07-10 14:36:32 +0100

    Added convert to Fahrenheit

--- src/WeatherTab.js
+++ src/WeatherTab.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherTab(props) {
   return (
@@ -15,10 +16,8 @@ export default function WeatherTab(props) {
         <div className="col-6">
           <div className="clearfix">
             <img src={props.data.iconUrl} alt={props.data.description} />
-            <span className="temperature">
-              {Math.round(props.data.temperature)}
-            </span>{" "}
-            <span className="units">°C</span>
+
+            <WeatherTemperature celsius={props.data.temperature} />
           </div>
         </div>
 
